a58b0d8edf62e1c57b0d9bcbf57c290c7ffb3b84
fermartin17 <femartin@itba.edu.ar>
2021-05-05 18:51:01 -0300

    verlet y beeman

--- tp4/src/main/java/grupo4/itba/edu/ar/Model/Particle.java
+++ tp4/src/main/java/grupo4/itba/edu/ar/Model/Particle.java
@@ -3,16 +3,18 @@ package grupo4.itba.edu.ar.Model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @Getter
 @Setter
 public class Particle {
-    private final double positionX;
-    private final double positionY;
-    private final double radius;
+    private final double currentPosition;
+    private List<Double> positionsHistory;
 
-    public Particle(double positionX, double positionY, double radius) {
-        this.positionX = positionX;
-        this.positionY = positionY;
-        this.radius = radius;
+    public Particle(double currentPosition) {
+        this.currentPosition = currentPosition;
+        this.positionsHistory = new LinkedList<>();
+        this.positionsHistory.add(currentPosition);
     }
 }
--- tp4/src/main/java/grupo4/itba/edu/ar/Oscilator/Oscilator.java
+++ tp4/src/main/java/grupo4/itba/edu/ar/Oscilator/Oscilator.java
@@ -1,5 +1,7 @@
 package grupo4.itba.edu.ar.Oscilator;
 
+import grupo4.itba.edu.ar.Model.Particle;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -11,14 +13,15 @@ public class Oscilator {
 
     public static class OscilatorMethods{
 
-        public static void oscillatorAnalitic() {
-            double mass = 70f; // kg
-            double k = 10000f; // constant N/m
-            double gamma = 100f; // kg/s
-            double tf = 5f; // s
-            double A = 2f; // TODO: cambiar. no se de donde sale esto
-            double deltaT = 0.00001f;
+        private static double mass = 70f; // kg
+        private static double k = 10000f; // constant N/m
+        private static double gamma = 100f; // kg/s
+        private static double tf = 5f; // s
+        private static double A = 2f; // TODO: cambiar. no se de donde sale esto
+        private static double deltaT = 0.00001f;
+        private static double springConstant = 1;
 
+        public static void oscillatorAnalitic() {
             LinkedList<Double> rs = new LinkedList<>();
             LinkedList<Double> vs = new LinkedList<>();
             double r = 1f; // m
@@ -34,7 +37,67 @@ public class Oscilator {
         }
 
         public static void oscilatorVerlet(){
+            LinkedList<Double> positions = new LinkedList<>();
 
+            double initialPosition = 1;
+            positions.add(initialPosition);
+
+            double initialVelocity = 0;
+
+            double nextPosition;
+            double nextVelocity;
+            double aceleration;
+
+            for(int i = 0; i < 1000000; i++ ){
+
+                aceleration = -(springConstant/mass) * positions.get(i);
+
+                if(i==0){
+                    nextVelocity = initialVelocity + aceleration * deltaT / mass;
+                    nextPosition = initialPosition + nextVelocity * deltaT + Math.pow(deltaT,2) * aceleration / (2*mass);
+                }
+                else
+                    nextPosition = 2 * positions.get(i) - positions.get(i - 1) + aceleration * Math.pow(deltaT,2) / mass;
+
+                positions.add(nextPosition);
+            }
+
+            saveOscillatorMovement(positions);
+        }
+
+
+        public static void oscilatorBeeman(){
+            LinkedList<Double> positions = new LinkedList<>();
+            LinkedList<Double> velocities = new LinkedList<>();
+            LinkedList<Double> acelerations = new LinkedList<>();
+
+
+            double initialPosition = 1;
+            positions.add(initialPosition);
+
+            double initialAceleration = -(springConstant/mass) * positions.get(0);
+            acelerations.add(initialAceleration);
+
+            double initialVelocity = acelerations.get(0) * deltaT;
+            velocities.add(initialVelocity);
+
+            double nextPosition = initialPosition + initialVelocity * deltaT;
+            positions.add(nextPosition);
+            double nextVelocity;
+            double nextAceleration = -(springConstant/mass) * positions.get(1);
+            acelerations.add(nextAceleration);
+
+            for(int i = 1; i < 1000000; i++ ){
+
+                nextPosition = positions.get(i) + velocities.get(i) * deltaT + (2/3) * acelerations.get(i) * Math.pow(deltaT,2) - (1/6) * acelerations.get(i - 1) * Math.pow(deltaT,2);
+                nextAceleration = -(springConstant/mass) * nextPosition;
+                nextVelocity = velocities.get(0) + (1/3) * nextAceleration * deltaT + (5/6) * acelerations.get(i) * deltaT - (1/6) * acelerations.get(i - 1) * Math.pow(deltaT,2);
+                positions.add(nextPosition);
+                velocities.add(nextVelocity);
+                acelerations.add(nextAceleration);
+            }
+
+            saveOscillatorMovement(positions);
         }
 
         private static void saveOscillatorMovement(List<Double> rs) {
