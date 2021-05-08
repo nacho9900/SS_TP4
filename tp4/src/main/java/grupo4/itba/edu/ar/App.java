f64a87f254a3151c49df9ddcf8862172a32ac0e2
sebas favaron <sfavaron@balanz.com>
2021-05-08 12:43:47 -0300

    ADD missing vel calculation

--- tp4/src/main/java/grupo4/itba/edu/ar/App.java
+++ tp4/src/main/java/grupo4/itba/edu/ar/App.java
@@ -1,6 +1,8 @@
 package grupo4.itba.edu.ar;
 
+import grupo4.itba.edu.ar.Model.Vector2;
 import grupo4.itba.edu.ar.Oscilator.Oscilator;
+import grupo4.itba.edu.ar.ParticlePropagation.ParticlePropagation;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -15,19 +17,19 @@ public class App
     public static void main( String[] args )
     {
         // Oscilator.OscilatorMethods.oscillatorAnalitic();
-         Oscilator.OscilatorMethods.oscilatorVerlet();
+        // Oscilator.OscilatorMethods.oscilatorVerlet();
         // Oscilator.OscilatorMethods.oscilatorBeeman();
 
-//        double D = Math.pow(10, -8);
-//        Vector2 v = new Vector2(Math.pow(10, 4), Math.pow(10, 5));
-//        double mass = Math.pow(10, -27);
-//        double dT = 0.001;
-//        ParticlePropagation particlePropagation = new ParticlePropagation(D, v, mass, dT);
-//        int i = 1000; // TODO: remove once idDone() is implemented
-//        while( !particlePropagation.isDone() && i-- > 0 ) {
-//            particlePropagation.nextStep();
-//        }
-//
-//        particlePropagation.saveMovement();
+       double D = Math.pow(10, -8);
+       Vector2 v = new Vector2(Math.pow(10, 4), Math.pow(10, 5));
+       double mass = Math.pow(10, -27);
+       double dT = 0.001;
+       ParticlePropagation particlePropagation = new ParticlePropagation(D, v, mass, dT);
+       int i = 3; // TODO: remove once idDone() is implemented
+       while( !particlePropagation.isDone() && i-- > 0 ) {
+           particlePropagation.nextStep();
+       }
+
+       particlePropagation.saveMovement();
     }
 }
